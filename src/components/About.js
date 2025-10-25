d3db01c9e0d35b62880904a121d290fc83746c28
deathehra <jarvidniies@gmail.com>
2025-10-25 13:03:36 +0530

    Rename TuneStation to DeathMusic and update content

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About(props) {
 
     useEffect(() => {
         document.body.scrollTop = document.documentElement.scrollTop = 0;
-        document.title = "About - TuneStation"
+        document.title = "About - DeathMusic"
     }, [])
 
     return (
@@ -13,8 +13,8 @@ function About(props) {
                 <div className="container px-5 pt-28 pb-10 mx-auto">
                     <div className="flex flex-col text-center w-full mb-20">
                         <h2 className="text-xs text-green-700 dark:text-green-400 tracking-widest font-medium title-font mb-1">USING UNOFFICIAL JIOSAAVN API</h2>
-                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-black dark:text-white">TuneStation</h1>
-                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">With TuneStation, you can listen and download over 80 million songs (number of songs available on JioSaavn) for FREE without any ads!</p>
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-black dark:text-white">DeathMusic</h1>
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Listen and download 80M+ songs free and ad-free with DeathMusic!</p>
                     </div>
                     <div className="flex flex-col text-center mb-2">
                         <h2 className="text-sm text-green-700 dark:text-green-400 tracking-widest font-medium title-font mb-1">FEATURES</h2>
@@ -62,65 +62,7 @@ function About(props) {
                                 <p className="leading-relaxed text-base">With TuneStation's intuitive search functionality, you can quickly and easily find your favorite songs, albums, and playlists,</p>
                             </div>
                         </div>
-                    </div>
-
-
-                    {/* Tools Used */}
-                    <div className="flex flex-col text-center mt-16 mb-1">
-                        <h2 className="text-sm text-green-700 dark:text-green-400 tracking-widest font-medium title-font mb-1">BUILT WITH</h2>
-                    </div>
-                    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2">
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-cyan-800 hover:bg-cyan-900 rounded flex p-4 h-full items-center">
-
-                                <a href="https://react.dev/" target="_blank" rel="noopener noreferrer" className="flex justify-center w-full title-font font-medium text-white">ReactJS</a>
-                            </div>
-                        </div>
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-sky-600 hover:bg-sky-800 rounded flex p-4 h-full items-center">
-
-                                <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer" className="flex justify-center w-full title-font font-medium text-white">TailwindCSS</a>
-                            </div>
-                        </div>
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-green-800 hover:bg-green-900 rounded flex p-4 h-full items-center">
-
-                                <a href="https://github.com/sumitkolhe/jiosaavn-api" target="_blank" rel="noopener noreferrer" className="flex justify-center w-full title-font font-medium  text-white">Sumit Kolhe's Unofficial JioSaavn API</a>
-                            </div>
-                        </div>
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-gray-800 hover:bg-gray-900 rounded flex p-4 h-full items-center">
-
-                                <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer" className="flex justify-center w-full title-font font-medium text-white">Hosted on Vercel</a>
-                            </div>
-                        </div>
-
-                    </div>
-                    {/* Links */}
-                    <div className="flex flex-col text-center mt-16 mb-1">
-                        <h2 className="text-sm text-green-700 dark:text-green-400 tracking-widest font-medium title-font mb-1">RELATED LINKS</h2>
-                    </div>
-                    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2">
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-pink-700 hover:bg-pink-900 rounded flex p-4 h-full items-center">
-
-                                <a href="https://github.com/codyandersan/" target="_blank" rel="noopener noreferrer" className="flex justify-center w-full title-font font-medium text-white">My Github Profile</a>
-                            </div>
-                        </div>
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-violet-700 hover:bg-violet-900 rounded flex p-4 h-full items-center">
-
-                                <a href="https://codyandersan.github.io/" target="_blank" rel="noopener noreferrer" className="flex justify-center w-full title-font font-medium text-white">My Other Projects</a>
-                            </div>
-                        </div>
-                        <div className="p-2  w-full">
-                            <div className="bg-yellow-700 hover:bg-yellow-900 rounded flex p-4 h-full items-center">
-
-                                <a href="https://github.com/codyandersan/TuneStation" target="_blank" rel="noopener noreferrer" className="flex justify-center w-full title-font font-medium  text-white">TuneStation's Git Repository</a>
-                            </div>
-                        </div>
-                        
-
+                   
                     </div>
                 </div>
 
